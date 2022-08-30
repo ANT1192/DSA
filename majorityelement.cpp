eb9bc9c4eec77827c3e2bd0256a9db18cb26fe9d
aditya <aaditripathi.nath@gmail.com>
2022-08-30 12:17:48 +0530

    Add files via upload

---
+++ majorityelement.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+      vector<int>v;
+        int n= nums.size();
+        unordered_map<int, int>mp;
+        for(int i=0;i<n;i++)
+        {
+            mp[nums[i]]++;
+}
+        for(auto &x : mp)
+        {
+            if(x.second>n/3)
+                v.emplace_back(x.first);
+}
+        return v;
+    }
+};
